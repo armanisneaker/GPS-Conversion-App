dadd174dd06e8d1c297a114226d0eb26709da069
Damian Szymanek <damian.szymanek.pl@gmail.com>
2023-06-08 05:39:00 +0200

    08_06_2023_05:38

--- GPS_Conversion_App.py
+++ GPS_Conversion_App.py
@@ -34,11 +34,9 @@ def calculate_coordinates(event=None):
     except Exception as e:
         messagebox.showerror("Error", str(e))
 
-# Create the main window
 window = tk.Tk()
 window.title("Coordinates Calculator")
 
-# Create input entry boxes
 labels = ["Latitude (deg):", "Latitude (min):", "Latitude (sec):", "Longitude (deg):", "Longitude (min):", "Longitude (sec):"]
 entry_boxes = []
 for i, label_text in enumerate(labels):
@@ -52,25 +50,19 @@ for i, label_text in enumerate(labels):
     # Bind Enter key to calculate_coordinates function
     entry.bind("<Return>", calculate_coordinates)
 
-# Create calculate button
 calculate_button = tk.Button(window, text="Calculate", command=calculate_coordinates)
 calculate_button.grid(row=len(entry_boxes), column=0, columnspan=2, padx=10, pady=10)
 
-# Create latitude result text widget
 latitude_result_text = st.ScrolledText(window, height=1, width=20)
 latitude_result_text.grid(row=len(entry_boxes) + 1, column=0, padx=10, pady=5)
 
-# Create longitude result text widget
 longitude_result_text = st.ScrolledText(window, height=1, width=20)
 longitude_result_text.grid(row=len(entry_boxes) + 1, column=1, padx=10, pady=5)
 
-# Create copy latitude button
 copy_latitude_button = ttk.Button(window, text="Copy Latitude", command=copy_latitude)
 copy_latitude_button.grid(row=len(entry_boxes) + 2, column=0, padx=10, pady=5)
 
-# Create copy longitude button
 copy_longitude_button = ttk.Button(window, text="Copy Longitude", command=copy_longitude)
 copy_longitude_button.grid(row=len(entry_boxes) + 2, column=1, padx=10, pady=5)
 
-# Start the GUI event loop
 window.mainloop()
